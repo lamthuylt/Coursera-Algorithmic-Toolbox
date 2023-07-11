0684a2febdbc67e9bf4672d3a86bd653d4dc3494
Thi-Lam-Thuy LE <lamthuy.lt@gmail.com>
2023-07-11 11:34:28 +0200

    edit solution 4.1

--- solutions/4.1_binary_search.py
+++ solutions/4.1_binary_search.py
@@ -9,28 +9,28 @@ def binary_search(keys, query, l, r):
     elif query == keys[mid]:
         return mid
     elif query < keys[mid]:
-        return binary_search(keys, query, l, mid)
+        return binary_search(keys, query, l, mid-1)
     elif query > keys[mid]:
         return binary_search(keys, query, mid+1, r)  
 
 if __name__ == '__main__':
     ### read inputs from file
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    f = open("4.1_input.json", "r")
-    lines = f.readlines()
-    num_keys = int(lines[0])
-    input_keys = list(map(int, lines[1].split()))
-    num_queries = int(lines[2])
-    input_queries = list(map(int, lines[3].split()))
+    # os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    # f = open("4.1_input.json", "r")
+    # lines = f.readlines()
+    # num_keys = int(lines[0])
+    # input_keys = list(map(int, lines[1].split()))
+    # num_queries = int(lines[2])
+    # input_queries = list(map(int, lines[3].split()))
 
     ### insert inputs by hands
-    # num_keys = int(input())
-    # input_keys = list(map(int, input().split()))
-    # assert len(input_keys) == num_keys
+    num_keys = int(input())
+    input_keys = list(map(int, input().split()))
+    assert len(input_keys) == num_keys
 
-    # num_queries = int(input())
-    # input_queries = list(map(int, input().split()))
-    # assert len(input_queries) == num_queries
+    num_queries = int(input())
+    input_queries = list(map(int, input().split()))
+    assert len(input_queries) == num_queries
 
     for q in input_queries:
         print(binary_search(input_keys, q, 0, num_keys-1), end=' ')
